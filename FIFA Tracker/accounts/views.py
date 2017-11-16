7e1f3f108837679d20b9db77d861514f40ee350c
xAranaktu <paweh10@gmail.com>
2017-11-16 15:01:03 +0100

    Working account creation and user login

--- FIFA Tracker/accounts/views.py
+++ FIFA Tracker/accounts/views.py
@@ -1,10 +1,14 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
 
 from .forms import SignUpForm
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     icons = {"username": "glyphicon-user", "email": "glyphicon-envelope", "password1": "glyphicon-lock", "password2": "glyphicon-lock"}
 
     if request.method == 'POST':
@@ -18,8 +22,19 @@ def signup(request):
     return render(request, 'signup.html', {'form': form, 'icons': icons })
 
 def login_view(request):
-    icons = {"username": "glyphicon-user", "password": "glyphicon-lock"}
-    
-    form = AuthenticationForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    icons = {"username": "glyphicon-user", "password": "glyphicon-lock"}    
+    if request.method == 'POST':
+        form = AuthenticationForm(None, request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
     
     return render(request, 'login.html', {'form': form, 'icons': icons })

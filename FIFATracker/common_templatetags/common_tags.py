9bc96d7b1da02afa6e47b074012f715c30de114d
xAranaktu <paweh10@gmail.com>
2018-07-23 17:08:35 +0200

    Added 'file_timestamp' tag

--- FIFATracker/common_templatetags/common_tags.py
+++ FIFATracker/common_templatetags/common_tags.py
@@ -1,4 +1,7 @@
+import os
+
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -17,3 +20,12 @@ def url_params(request, *args):
             field=str(param), field_value=str(request_dict[param]))
 
     return params.replace(',', "%2C")
+
+
+@register.simple_tag
+def file_timestamp(path):
+    full_path = os.path.join(settings.STATIC_ROOT, path)
+    if not os.path.exists(full_path):
+        return 1
+
+    return int(os.path.getmtime(full_path))

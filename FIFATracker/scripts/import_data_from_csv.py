8f5cf155a29b85b32c22ac5a91e161bda75736a4
xAranaktu <paweh10@gmail.com>
2018-10-21 18:36:26 +0200

    import data from "Database Data" folder

--- FIFATracker/scripts/import_data_from_csv.py
+++ FIFATracker/scripts/import_data_from_csv.py
@@ -3,13 +3,13 @@ import os
 from django.conf import settings
 from django.db import connection
 
-
+# python manage.py runscript import_data_from_csv --script-args nations19.csv datanations19
 def run(*args):
     fname = args[0]
     table = args[1]
 
     # FULL PATH TO CSV FILE
-    csv_file = os.path.join(settings.BASE_DIR, "scripts", "Data", fname)
+    csv_file = os.path.join(settings.BASE_DIR, "Database Data", fname)
 
     with connection.cursor() as cur:
         with open(csv_file, 'r', encoding='utf-8') as f:

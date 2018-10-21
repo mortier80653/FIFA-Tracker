8c25581ac9e0353fd2e4e43bc10109e9a06abdc4
xAranaktu <paweh10@gmail.com>
2018-10-21 18:48:59 +0200

    import data from "Database Data" folder

--- FIFATracker/scripts/import_data_from_csv.py
+++ FIFATracker/scripts/import_data_from_csv.py
@@ -9,7 +9,7 @@ def run(*args):
     table = args[1]
 
     # FULL PATH TO CSV FILE
-    csv_file = os.path.join(settings.BASE_DIR, "Database Data", fname)
+    csv_file = os.path.join(settings.BASE_DIR, "..", "Database Data", fname)
 
     with connection.cursor() as cur:
         with open(csv_file, 'r', encoding='utf-8') as f:
